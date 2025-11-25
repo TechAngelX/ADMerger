7f9ff65577adaf49699646329568d8eee5ebefdd
Ricki Angel <angelricki@outlook.com>
2025-11-25 17:58:43 +0000

    update: Create 'skeleton' outline architecture for Service modules.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,16 @@
-// Program.cs
-
-using System;
+﻿using System;
 using System.Windows.Forms;
-using ADMerger.Services;
 
 namespace ADMerger
 {
-    internal class Program
+    static class Program
     {
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            var csvService = new CsvService();
-            var equivalencyService = new EquivalencyService();
-            var matchingService = new InstitutionMatchingService();
-            var rankingService = new RankingService(matchingService);
-            var gradeService = new GradeClassificationService(equivalencyService);
-            
-            var mainForm = new MainForm(
-                csvService,
-                equivalencyService,
-                rankingService,
-                gradeService
-            );
-            
-            Application.Run(mainForm);
+            Application.Run(new MainForm());
         }
     }
 }
